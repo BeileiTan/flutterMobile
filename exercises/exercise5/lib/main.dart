c90c2550080c1925f5ce042b3c198e1f3973461c
BeileiTan <beilei0508@gmail.com>
2024-04-19 21:43:31 -0700

    This is exercise 5

---
+++ exercises/exercise5/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:exercise5/MyHomePage.dart';
+import 'package:camera/camera.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
